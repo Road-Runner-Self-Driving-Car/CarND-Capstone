70843abd3b335925d803d0a23f140a9d9c0fecc2
LT Wang <hyperionwlt@gmail.com>
2018-05-20 16:47:27 -0700

    Change the controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,13 +12,13 @@ class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         # TODO: Implement
 
-        kp = 0.9
+        kp = 0.1
         ki = 0.0005
-        kd = 0.07
+        kd = 1.0
         mn = decel_limit
         mx = accel_limit
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
-		self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+        self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         tau = 0.2
         ts = 0.1
         self.vel_lpf = LowPassFilter(tau,ts)
@@ -39,32 +39,38 @@ class Controller(object):
 
        	if not dbw_enabled:
        		self.throttle_controller.reset()
+			self.last_time = rospy.get_time()
        		return 0., 0., 0.,
        	# current_vel = self.vel_lpf.filt(current_vel)
-    	steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+    	current_time = rospy.get_time()
+    	sample_time = current_time - self.last_time
+    	self.last_time = current_time
 
     	vel_error = linear_vel - current_vel
     	self.last_vel = current_vel
-
-    	current_time = rospy.get_time()
-    	sample_time = current_time - self.last_time
-    	self.last_time  = current_time
-
-	    throttle = self.throttle_controller.step(vel_error, sample_time)
-		throttle = self.vel_lpf.filt(throttle)
-    	
-		brake = 0.0
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        #throttle = self.vel_lpf.filt(throttle)
+		if throttle > self.accel_limit:
+			throttle = self.accel_limit
+		if throttle < self.decel_limit:
+			throttle = self.decel_limit
+        brake = 0.0
     	# Here to set the velocity threshold so that, as the speed is too low, just stop
     	# In order to avoid the jerk
-    	if abs(linear_vel)  < 0.01  and abs(current_vel) < 0.3:
-    		throttle = self.decel_limit
-
-    	if throttle < 0:
-			decel = abs(throttle)
+    	if abs(linear_vel)  < -self.brake_deadband:
+			brake = -throttle * self.vehicle_mass*self.wheel_radius
     		throttle = 0
-    		# decel = max(vel_error, self.decel_limit)
-    		brake = decel *self.vehicle_mass*self.wheel_radius if decel > 0 else 0
+		elif throttle < self.brake_deadband:
+			brake = 0
+			throttle = 0
+    	else:
+			brake = 0
 
+    		# decel = max(vel_error, self.decel_limit)
+            brake = decel *self.vehicle_mass*self.wheel_radius if decel > 0 else 0
+
+
+		steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
     	# if vel_error == 0 and throttle == 0:
     	# 	brake = 0.0
 
--- ros/src/twist_controller/yaw_controller.py
+++ ros/src/twist_controller/yaw_controller.py
@@ -1,7 +1,7 @@
 from math import atan
 import numpy as numpy
 from pid import PID
-frow lowpass import LowPassFilter
+from lowpass import LowPassFilter
 
 class YawController(object):
     def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
