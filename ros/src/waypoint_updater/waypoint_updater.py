6cfe43314834fe1a1545e84bc6ea2c91f436c46d
hyperionWang <hyperionwlt@gmail.com>
2018-05-21 05:19:00 -0600

    Fix the bug...

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -26,7 +26,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200  # Number of waypoints we will publish. You can change this number
-MAX_DECELERATION = 0.6
+MAX_DECELERATION = 0.7
 STOP_DISTANCE = 2
 
 
