ba99cf92cde5209d8ae48cc66092ffba66be094d
hyperionWang <hyperionwlt@gmail.com>
2018-05-21 04:26:24 -0600

    Fix the bug...

--- ros/src/twist_controller/yaw_controller.py
+++ ros/src/twist_controller/yaw_controller.py
@@ -13,7 +13,7 @@ class YawController(object):
         self.min_angle = -max_steer_angle
         self.max_angle = max_steer_angle
         self.linear_pid = PID(0.9, 0.001, 0.0004, self.min_angle, self.max_angle)
-        self.cte_pid = PID(0.8, 0, 0.4, self.min_angle, self.max_angle)
+        self.cte_pid = PID(0.8, 0, 0.25, self.min_angle, self.max_angle)
         self.tau = 0.2
         self.ts = 0.1
         self.lpf = LowPassFilter(self.tau, self.ts)
@@ -25,7 +25,7 @@ class YawController(object):
 
     def get_steering(self, linear_velocity, angular_velocity, current_velocity, cur_angular, sample_time):
         angular_velocity = current_velocity * angular_velocity / linear_velocity if abs(linear_velocity) > 0. else 0.
-        angular_velocity = self.cte_pid.step(angular_velocity - cur_angular, sample_time)
+        angular_velocity = self.cte_pid.step(angular_velocity, sample_time)
         if abs(current_velocity) > 0.1:
             max_yaw_rate = abs(self.max_lat_accel / current_velocity);
             angular_velocity = max(-max_yaw_rate, min(max_yaw_rate, angular_velocity))
